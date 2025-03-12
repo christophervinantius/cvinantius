f839556335bd65f80f3f222dbd901b4fbb8f0572
Christopher Vinantius <christopher.vinantius@binus.ac.id>
2025-03-12 16:17:02 +0700

    update cv

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -6,7 +6,7 @@ function Hero() {
         <div id="hero-section" className="py-8 flex flex-col items-center justify-center text-center gap-4">
             <div className="flex flex-col gap-4 text-3xl font-bold text-red-800">
                 <h1>Christopher Vinantius</h1>
-                <a className="text-lg font-light p-1 bg-white text-red-800 rounded-xl border-3 border-red-800 hover:bg-red-800 hover:text-white transition duration-500 ease-in-out" href="assets/CV/CV_ChristopherVinantius.pdf" target="_blank" rel="noreferrer">See my Curriculum Vitae</a>
+                <a className="text-lg font-light p-1 bg-white text-red-800 rounded-xl border-3 border-red-800 hover:bg-red-800 hover:text-white transition duration-500 ease-in-out" href="assets/CV/CV_ChristopherVinantius.pdf" target="_blank" rel="noreferrer">Check out my Curriculum Vitae!</a>
             </div>
             <div className="w-4/5 flex flex-col gap-4 text-sm md:text-lg bg-red-800 text-white p-6 shadow-xl rounded-xl">
                 <p>I am a Computer Science graduate from Bina Nusantara University living in Jakarta, Indonesia.</p>
